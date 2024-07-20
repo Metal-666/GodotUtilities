a06e2515744b5e55e00da0adbe36d4cc6283d3c8
Metal <50769245+Metal-666@users.noreply.github.com>
2024-07-20 19:11:03 +0300

    Changed Logging extensions to allow logging from any object

--- GodotUtilities/Extensions/Logging.cs
+++ GodotUtilities/Extensions/Logging.cs
@@ -1,6 +1,4 @@
-﻿using Godot;
-
-using Metal666.GodotUtilities.Logging;
+﻿using Metal666.GodotUtilities.Logging;
 
 using System;
 using System.Collections.Generic;
@@ -12,7 +10,7 @@ public static class Logging {
 
 	public static int Indent { get; set; }
 
-	public static void Log(this Node node,
+	public static void Log(this object source,
 							LogLevel logLevel,
 							IndentAction indentAction,
 							params string[] messages) {
@@ -56,7 +54,7 @@ public static class Logging {
 
 			LogManager.Log(message,
 							logLevel,
-							node.GetType(),
+							source.GetType(),
 							Indent);
 
 		}
@@ -92,84 +90,84 @@ public static class Logging {
 
 	}
 
-	public static void LogMessage(this Node node,
+	public static void LogMessage(this object source,
 									string message,
 									IndentAction indentAction = IndentAction.None) =>
-		Log(node,
+		Log(source,
 			LogLevel.Message,
 			indentAction,
 			message);
 
-	public static void LogWarning(this Node node,
+	public static void LogWarning(this object source,
 									string message,
 									IndentAction indentAction = IndentAction.None) =>
-		Log(node,
+		Log(source,
 			LogLevel.Warning,
 			indentAction,
 			message);
 
-	public static void LogError(this Node node,
+	public static void LogError(this object source,
 								string message,
 								IndentAction indentAction = IndentAction.None) =>
-		Log(node,
+		Log(source,
 			LogLevel.Error,
 			indentAction,
 			message);
 
-	public static void LogIndent(this Node _) =>
+	public static void LogIndent(this object _) =>
 		Indent++;
-	public static void LogUnindent(this Node _) =>
+	public static void LogUnindent(this object _) =>
 		Indent--;
 
 	public static IEnumerable<T> Log<T>(this IEnumerable<T> source,
-										Node node,
+										object logSource,
 										string message,
 										LogLevel logLevel,
 										IndentAction indentAction = IndentAction.None) {
 
-		Log(node, logLevel, indentAction, message);
+		Log(logSource, logLevel, indentAction, message);
 
 		return source;
 
 	}
 
 	public static IEnumerable<T> LogMessage<T>(this IEnumerable<T> source,
-												Node node,
+												object logSource,
 												string message,
 												IndentAction indentAction = IndentAction.None) =>
 		Log(source,
-			node,
+			logSource,
 			message,
 			LogLevel.Message,
 			indentAction);
 
 	public static IEnumerable<T> LogWarning<T>(this IEnumerable<T> source,
-												Node node,
+												object logSource,
 												string message,
 												IndentAction indentAction = IndentAction.None) =>
 		Log(source,
-			node,
+			logSource,
 			message,
 			LogLevel.Warning,
 			indentAction);
 
 	public static IEnumerable<T> LogError<T>(this IEnumerable<T> source,
-												Node node,
+												object logSource,
 												string message,
 												IndentAction indentAction = IndentAction.None) =>
 		Log(source,
-			node,
+			logSource,
 			message,
 			LogLevel.Error,
 			indentAction);
 
 	public static IEnumerable<T> LogForEach<T>(this IEnumerable<T> source,
-												Node node,
+												object logSource,
 												LogLevel logLevel,
 												IndentAction indentAction,
 												Func<T, string> action) {
 
-		Log(node,
+		Log(logSource,
 			logLevel,
 			indentAction,
 			source.Select(action)
@@ -180,56 +178,56 @@ public static class Logging {
 	}
 
 	public static IEnumerable<T> LogMessageForEach<T>(this IEnumerable<T> source,
-														Node node,
+														object logSource,
 														IndentAction indentAction,
 														Func<T, string> action) =>
 		LogForEach(source,
-					node,
+					logSource,
 					LogLevel.Message,
 					indentAction,
 					action);
 
 	public static IEnumerable<T> LogMessageForEach<T>(this IEnumerable<T> source,
-														Node node,
+														object logSource,
 														Func<T, string> action) =>
 		LogMessageForEach(source,
-							node,
+							logSource,
 							IndentAction.None,
 							action);
 
 	public static IEnumerable<T> LogWarningForEach<T>(this IEnumerable<T> source,
-														Node node,
+														object logSource,
 														IndentAction indentAction,
 														Func<T, string> action) =>
 		LogForEach(source,
-					node,
+					logSource,
 					LogLevel.Warning,
 					indentAction,
 					action);
 
 	public static IEnumerable<T> LogWarningForEach<T>(this IEnumerable<T> source,
-														Node node,
+														object logSource,
 														Func<T, string> action) =>
 		LogWarningForEach(source,
-							node,
+							logSource,
 							IndentAction.None,
 							action);
 
 	public static IEnumerable<T> LogErrorForEach<T>(this IEnumerable<T> source,
-													Node node,
+													object logSource,
 													IndentAction indentAction,
 													Func<T, string> action) =>
 		LogForEach(source,
-					node,
+					logSource,
 					LogLevel.Warning,
 					indentAction,
 					action);
 
 	public static IEnumerable<T> LogErrorForEach<T>(this IEnumerable<T> source,
-													Node node,
+													object logSource,
 													Func<T, string> action) =>
 		LogErrorForEach(source,
-						node,
+						logSource,
 						IndentAction.None,
 						action);
 
